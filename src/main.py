d1d0bc8f2e3b95ad50f950a5f3f81cd63ba83d23
Alivia321 <34127453+Alivia321@users.noreply.github.com>
2025-07-06 22:33:36 -0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,2 +1,11 @@
+from secondary import square_job
 if __name__ == "__main__":
-    print("hello world")
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("arg1", type=int, help="Integer to square")
+    parser.add_argument("--delay", type=float, default=0, help="Optional delay in seconds")
+    args = parser.parse_args()
+
+    print("Running main.py", flush=True)
+    square_job(args.arg1, args.delay)
